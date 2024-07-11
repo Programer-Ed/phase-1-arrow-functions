d86824027cbff835eeb7f09ea41fa4736af6ba0b
Edwin <moyiedwin8@gmail.com>
2024-07-11 17:03:56 +0300

    adds arrows

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide(num1,num2){
+    return num1 / num2
+}
+divide(2000/100)
+function square(num3){
+    return num3 * num3
+}
+square(4)
+function add(num4,num5){
+     return num4 + num5
+}
+add(1,2)
